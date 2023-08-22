054e77d691eb3689b94b7f7d6588820ff7d540ce
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-08-22 13:48:58 +0200

    Added today var, added days var for "aliasing"

--- js/index.js
+++ js/index.js
@@ -5,18 +5,28 @@ const API_KEY = config.MY_API_TOKEN;
 const API_BASE_URL =
 	"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
+const DAYS = {
+	0: "Sunday",
+	1: "Monday",
+	2: "Tuesday",
+	3: "Wednesday",
+	4: "Thursday",
+	5: "Friday",
+	6: "Saturday",
+};
+
 const currentDate = new Date();
+const today = currentDate.getDay();
 const dateIn7Days = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
 const formattedCurrentDate = currentDate.toISOString().substring(0, 19);
 const formattedDateIn7Days = dateIn7Days.toISOString().substring(0, 19);
 
-console.log(formattedCurrentDate);
-console.log(formattedDateIn7Days);
+console.log(today);
 
-async function getWeeklyData() {
+async function getWeatherData() {
 	const [error, response] = await tryToCatch(
 		fetch,
-		`${API_BASE_URL}Haugesund,Norway/${formattedCurrentDate}/${formattedDateIn7Days}?ksey=${API_KEY}`,
+		`${API_BASE_URL}Haugesund,Norway/${formattedCurrentDate}/${formattedDateIn7Days}?key=${API_KEY}&elements=temp,humidity,windspeed,conditions,icon,datetime`,
 	);
 	if (error) {
 		console.log(error);
@@ -30,4 +40,4 @@ async function getWeeklyData() {
 	return data;
 }
 
-getWeeklyData();
+// getWeatherData();
